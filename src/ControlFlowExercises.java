809efbae36727320da1144d5f6b714059e735add
RiverRobins <rivergrahamrobins@gmail.com>
2019-11-21 11:32:59 -0600

    did things

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,2 +1,50 @@
+import java.util.Scanner;
 public class ControlFlowExercises {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.useDelimiter("\n");
+
+
+        Integer num = 100;
+        while (num >= -10){
+            System.out.print(num + " ");
+            num -= 5;
+        }
+
+        num = 0;
+        do {
+            System.out.println(num);
+            num += 2;
+        } while (num >= 100);
+
+        num = 0;
+        for (int i = 100; i > -10; i -= 5) {
+            System.out.println(i);
+        }
+        for (int i = 0; i < 100; i += 2) {
+            System.out.println(i);
+        }
+
+        for (int i = 0; i < 100; i++) {
+            if (i % 3 == 0 & i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            }
+            else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            }
+            else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            }
+            else {
+                System.out.println(i);
+            }
+        }
+
+        System.out.println("Enter a Number to see squares");
+        num = scanner.nextInt();
+        scanner.nextLine();
+
+        
+    }
 }
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -39,42 +39,5 @@ public class HelloWorld {
         System.out.println(bigByteOfBorg);
 
 
-        double pi = 3.14159;
-        System.out.printf("In math, pi is %.3f%n",pi);
-
-        Scanner scanner = new Scanner(System.in);
-
-        scanner.useDelimiter("\n");
-
-        System.out.println("Enter something: ");
-        String userInput = scanner.nextLine();
-        System.out.println("You entered: --> \"" + userInput + "\" <--");
-
-        System.out.println("Enter an integer");
-        int inNum = scanner.nextInt();
-        System.out.println(inNum);
-
-        scanner.nextLine();
-        System.out.println("Enter 3 things");
-        String in = scanner.nextLine();
-        String inArray[] = in.split(" ");
-//        System.out.println(inArray);
-
-        String word1 = inArray[0];
-        String word2 = inArray[1];
-        String word3 = inArray[2];
-
-        System.out.printf("You entered %s, %s, and %s", inArray[0], inArray[1], inArray[2]);
-
-        System.out.println("");
-
-        System.out.println("Enter length and width in the format (L W)");
-        String metrics = scanner.nextLine();
-        String inMetrics[] = metrics.split(" ");
-
-        float length = Float.parseFloat(inMetrics[0]);
-        float width = Float.parseFloat(inMetrics[1]);
-        System.out.printf("Area is %s. Perimeter is %s", (length * width),((length * 2) + (width * 2)));
-
     }
 }
