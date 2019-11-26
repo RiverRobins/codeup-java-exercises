12fc9703aec9c6d03658aa6c96668019e0931f49
RiverRobins <rivergrahamrobins@gmail.com>
2019-11-26 13:24:59 -0600

    that was the most poorly worded problem in this entire course

--- src/Person.java
+++ src/Person.java
@@ -12,11 +12,38 @@ public class Person {
     public void sayHello(){
         System.out.println("Hello, I am " + this.name);
     };
+    public Person(String name){
+        this.name = name;
+    }
 
     public static void main(String[] args){
-        Person dude = new Person();
+        Person dude = new Person("Boomers4Communism");
         dude.setName("Boomer");
         System.out.println(dude.getName());
         dude.sayHello();
+
+        System.out.println("");
+
+        Person person1 = new Person("John");
+        Person person2 = new Person("John");
+        System.out.println(person1.getName().equals(person2.getName()));
+        System.out.println(person1 == person2);
+
+        System.out.println("");
+
+        Person person3 = new Person("John");
+        Person person4 = person3;
+        System.out.println(person3 == person4);
+
+        System.out.println("");
+
+        Person person5 = new Person("John");
+        Person person6 = person1;
+        System.out.println(person5.getName());
+        System.out.println(person6.getName());
+        person6.setName("Jane");
+        System.out.println(person5.getName());
+        System.out.println(person6.getName());
+
     }
 }
---
+++ src/util/InputTest.java
@@ -0,0 +1,11 @@
+package util;
+
+public class InputTest {
+    public static void main(String[] args) {
+        Input input = new Input();
+        System.out.println(input.yesNo());
+        System.out.println(input.getString());
+        System.out.println(input.getInt(5, 25));
+        System.out.println(input.getDouble(1.5, 12.1));
+    }
+}
