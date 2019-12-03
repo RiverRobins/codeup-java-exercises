613575b0e9e44eb82f01cd984f8bf4e2ccde63d0
RiverRobins <rivergrahamrobins@gmail.com>
2019-12-03 11:19:36 -0600

    overrode square

--- src/util/shapes/Square.java
+++ src/util/shapes/Square.java
@@ -4,4 +4,10 @@ public class Square extends Rectangle {
     Square(double side){
         super(side, side);
     }
+    public double getArea(){
+        return len * len;
+    }
+    public double getPerimeter(){
+        return len * 4;
+    }
 }
